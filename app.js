58578e8f1842439b26463ffdeaa86a87af75d1de
luisfernandocastro <rockstartplus@gmail.com>
2025-08-06 23:30:40 -0500

    Crea la funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo(){
+    
+    let nombreAmigo = document.querySelector('input');
+    let textoMensaje = document.getElementById('mensajeError');
+
+    if (!nombreAmigo.value == " ") {
+
+        if(/^[a-zA-ZÁÉÍÓÚáéíóúñÑ\s]+$/.test(nombreAmigo.value)){
+
+            amigos.push(nombreAmigo.value);
+            console.log(amigos);
+            mensaje(textoMensaje, '');
+        }else{
+            //alert("Solo se permiten letras");
+            mensaje(textoMensaje, 'Solo se permiten letras');
+        }
+    }else{
+        //alert("Por favor, inserte un nombre");
+        mensaje(textoMensaje, 'Por favor, inserte un nombre');
+
+    }
+    nombreAmigo.value = '';
+
+}
+
+
+function mensaje(elemento, texto){
+
+    elemento.innerHTML = texto;
+    
+}
