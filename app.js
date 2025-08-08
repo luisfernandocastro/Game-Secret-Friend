79a2b634eddda92f09734bdcdf3c8063b1766f95
luisfernandocastro <rockstartplus@gmail.com>
2025-08-07 22:45:58 -0500

    logica terminada

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 // Aquí deberás desarrollar la lógica para resolver el problema.
 
 let amigos = [];
+let textoMensaje = document.getElementById('mensajeError');
+const listaNombres = document.getElementById('listaAmigos');
+const resultadoAmigoSorteado= document.getElementById('amigoSorteado');
+
 
 function agregarAmigo(){
     
     let nombreAmigo = document.querySelector('input');
-    let textoMensaje = document.getElementById('mensajeError');
 
     if (!nombreAmigo.value == " ") {
 
@@ -15,6 +18,13 @@ function agregarAmigo(){
             amigos.push(nombreAmigo.value);
             console.log(amigos);
             mensaje(textoMensaje, '');
+
+            nombreAmigo.focus();
+
+            document.getElementById('button-draw').removeAttribute('disabled'); 
+
+            actualizarListaAmigos();
+
         }else{
             //alert("Solo se permiten letras");
             mensaje(textoMensaje, 'Solo se permiten letras');
@@ -25,6 +35,62 @@ function agregarAmigo(){
 
     }
     nombreAmigo.value = '';
+
+}
+
+function actualizarListaAmigos(){
+
+
+    listaNombres.innerHTML = '';
+
+    for (const nombreAmigo of amigos) {
+
+        const nuevoelemento = document.createElement('li');
+        
+        nuevoelemento.textContent = nombreAmigo;
+
+        listaNombres.appendChild(nuevoelemento);
+
+        
+    }
+
+}
+
+
+function sortearAmigo(){
+    
+    if (amigos.length > 0){
+        let amigoSeleccionado = Math.floor(Math.random() * amigos.length) + 1;
+
+        resultadoAmigoSorteado.innerHTML = `El amigo Secreto sorteado es ${amigos[amigoSeleccionado-1]}`;
+
+        console.log(amigoSeleccionado);
+
+        listaNombres.innerHTML = '';
+
+        document.getElementById('container-btn-reiniciar').classList.remove('botonOculto');
+        document.getElementById('button-draw').setAttribute('disabled', 'true');
+        document.getElementById('button-add').setAttribute('disabled', 'true');
+
+    }else{
+        mensaje(textoMensaje, 'No hay amigos para sortear');
+    }
+}
+
+
+function reiniciarJuego(){
+    // borrar mensaje
+    resultadoAmigoSorteado.innerHTML = '';
+
+    // quitar boton reiniciar
+    document.getElementById('container-btn-reiniciar').classList.add('botonOculto');
+
+
+    // limpiar lista
+    amigos.length = 0;
+
+    // activar boton añadir
+    document.getElementById('button-add').removeAttribute('disabled');
 
 }
 
